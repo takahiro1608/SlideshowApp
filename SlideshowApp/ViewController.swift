9f5525c6b1151902585ad662b58ac5774949f1bc
鈴木貴広 <takahiro.suzuki@gmail.com>
2017-01-26 18:09:18 +0900

    ・メンタリング(01/16 22:00-)での質問箇所のアップデート
    ・戻るボタンの機能の実装　途中
    ・画像の拡大画面への画面遷移　途中

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,60 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-   
+    let imageArray = [
+        #imageLiteral(resourceName: "image01"),
+        #imageLiteral(resourceName: "image02"),
+        #imageLiteral(resourceName: "image03"),
+        #imageLiteral(resourceName: "image04")
+    ]
+    var imageIndex = 0
+    
+    var timer: Timer? = nil
     
     @IBOutlet weak var image: UIImageView!
-   
+    
     /* var playButton: UIButton! = 再生*/
-     
+    
     @IBAction func pushPlayButton(_ sender: Any) {
-        
-        var timer: Timer? = nil
         if timer == nil {
-            // button play
-            // アニメーションを開始
-            image.startAnimating()
-           //pushPlayButton.text = "停止"
-            
+            timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(self.pushNextButton(_:)), userInfo: nil, repeats: true)
+            timer?.fire()
         } else {
-            // button stop
-            // アニメーションを終了
-            image.stopAnimating()
+            timer?.invalidate()
+            timer = nil
         }
-        timer = nil
     }
- 
+    
     @IBAction func pushNextButton(_ sender: Any) {
+        if imageIndex == imageArray.count - 1 {
+            imageIndex = 0
+        } else {
+            imageIndex += 1
+        }
+        image.image = imageArray[imageIndex]
     }
     @IBAction func pushPreviousButton(_ sender: Any) {
+        if imageIndex == imageArray.count - 1 {
+            imageIndex = 3
+        } else {
+            imageIndex -= 1
+        }
+        image.image = imageArray[imageIndex]
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        func slideImages () -> Array<UIImage> {
-            var theArray = Array<UIImage>()
-            for num in 1...4 {
-                // slideのイメージを作る
-                let imageName = "image0" + String(num)
-                let image = UIImage(named: imageName)
-                // 配列に追加する
-                theArray.append(image!)
-            }
-            return theArray
-        }
-
-       // if !image.isAnimating {
-        // 画像の配列をアニメーションにセット
-        image.animationImages = slideImages()
-        // 2秒間隔 ✕ 画像4枚
-        image.animationDuration = 8
-        // 3回繰り返し
-        image.animationRepeatCount = 3
-        // アニメーションをスタートする
-        image.startAnimating()
-        //}
+        //        func slideImages () -> Array<UIImage> {
+        //            var theArray = Array<UIImage>()
+        //            for num in 1...4 {
+        //                // slideのイメージを作る
+        //                let imageName = "image0" + String(num)
+        //                let image = UIImage(named: imageName)
+        //                // 配列に追加する
+        //                theArray.append(image!)
+        //            }
+        //            return theArray
+        //        }
+        //
+        //       // if !image.isAnimating {
+        //        // 画像の配列をアニメーションにセット
+        //        image.animationImages = slideImages()
+        //        // 2秒間隔 ✕ 画像4枚
+        //        image.animationDuration = 8
+        //        // 3回繰り返し
+        //        image.animationRepeatCount = 3
+        //        // アニメーションをスタートする
+        //        image.startAnimating()
+        //        //}
+        //
         
+        image.image = imageArray[imageIndex]
         
     }
     
@@ -70,7 +85,11 @@ class ViewController: UIViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         //print("touchesBegan")//
         performSegue(withIdentifier: "largerImage", sender: nil)
-        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let controller = segue.destination as! ImageViewController
+        // controller.image = imageArray[imageIndex]
     }
     
     @IBAction func unwind(segue: UIStoryboardSegue) {
@@ -80,6 +99,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
 
